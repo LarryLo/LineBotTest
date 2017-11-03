5a89590ae44cee9ffc1d5f0156989bbcee9a4343
cIII1993 <jefferychen16@gmail.com>
2017-11-03 20:44:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -318,7 +318,7 @@ function claculate(inputStr){
   while(tempMatch.match(/\d+\*\d+/)!=null){
     //b[0]*b[1]=a
     let a = tempMatch.match(/\d+\*\d+/).toString();
-    let b = a.match(/\d+/i);
+    let b = a.match(/\d+/g);
     return b[0].toString;
     let c = b[0]*b[1];
     tempMatch=tempMatch.replace(a,c.toString());
