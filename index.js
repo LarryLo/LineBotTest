fdbe9bd625da385942a21f4d203879956b48d65f
cIII1993 <jefferychen16@gmail.com>
2017-11-03 11:51:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -86,12 +86,99 @@ function parseInput(rplyToken, inputStr) {
   let countStr = '';
   let msgSplitor = ' ';
   let mainMsg = inputStr.split(msgSplitor);
+  let trigger = mainMsg[0].toString;
   //Kx
   if (trigger.match(/^(k)(\d+)((\+|-)\d+)?(@\d+)?(\$(\+|-)?\d+)?$/)!= null ){
-    return Kx(trigger);}
+    return Kx(trigger);
+  }
   //gr
   if (trigger.match(/^gr/)!= null ){
     return swGr();
   }
   return countStr;
+}
+//SW2.0指令
+function Kx(triggermsg) {
+  let returnStr = 'SW2.0威力表擲骰：\n';
+  let tempMatch = triggermsg.match(/^(k)(\d+)((\+|-)\d+)?(@\d+)?(\$(\+|-)?\d+)?$/)[0].toString();
+  //return tempMatch.match(/k\d+/).toString();
+  let k=0;
+  let b=0;
+  let c=10;
+  let s=0;
+  let sFlag=false;
+  let count=0;
+  let damage=0;
+  let dice=0;
+  let dice1=0;
+  let dice2=0;
+  if(tempMatch.match(/k\d+/)!=null){
+    k=tempMatch.match(/k\d+/).toString();
+    k=k.match(/\d+/).toString();
+  }
+  if(tempMatch.match(/(\+|-)\d+/)!=null){
+    b=tempMatch.match(/(\+|-)\d+/)[0].toString();
+    b=b.match(/-?\d+/).toString();
+  }
+  if(tempMatch.match(/@\d+/)!=null){
+    c=tempMatch.match(/@\d+/).toString();
+    c=c.match(/\d+/).toString();
+  }
+  if(tempMatch.match(/\$(\+|-)?\d+/)!=null){
+    s=tempMatch.match(/\$(\+|-)?\d+/)[0].toString();
+    if(tempMatch.match(/(\+|-)/)==null)
+      sFlag=true;
+    s=s.match(/-?\d+/).toString();
+  }
+  if(c<7){
+    returnStr+='c值不能小於7喔';
+    return returnStr;
+  }
+  if(k<0||k>100){
+    returnStr+='k值只支援0~100喔';
+    return returnStr;
+  }
+  dice1= Math.ceil(Math.random()*6);dice2= Math.ceil(Math.random()*6);
+  if(sFlag){
+    dice=Number(s);
+    returnStr+='['+dice+']';
+  }
+  else{
+    dice=dice1+dice2+Number(s);
+    returnStr+='['+dice1+','+dice2+']';
+    if(s>0)returnStr+='+'+s;
+    else if(s<0)returnStr+=+s;
+  }
+  if(dice>12){
+    damage+=powerSheet[k][9];
+  }
+  else if(dice>2){
+    damage+=powerSheet[k][dice-3];
+  }
+  else{
+    return returnStr+'→☆大失敗☆ 回家領50囉～';
+  }
+  while(dice>=c){
+    count++;
+    dice1= Math.ceil(Math.random()*6);
+    dice2= Math.ceil(Math.random()*6);
+    dice=dice1+dice2;returnStr+=',['+dice1+','+dice2+']';
+    if(dice>2){
+      damage+=powerSheet[k][dice-3];
+    }
+  }
+  damage=damage+Number(b);
+  if(count){
+    returnStr=returnStr+'→'+count+'迴轉→'+damage;
+  }
+  else{
+    returnStr=returnStr+'→'+damage;
+  }
+  return returnStr;}
+
+//成長骰
+function swGr() {
+  let returnStr = 'SW2.0成長擲骰：\n';
+  returnStr+='['+swGrSheet[Math.floor(Math.random()*6)]+', '+swGrSheet[Math.floor(Math.random()*6)]+']';
+  return returnStr;
 }
