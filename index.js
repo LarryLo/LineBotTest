0188e44995cd6c3a93fe7d37893d5c385024cf74
cIII1993 <jefferychen16@gmail.com>
2017-11-03 18:48:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -305,6 +305,7 @@ function claculater(inputStr){
   }
   while(tempMatch.match(/\([^(]+\)/) != null){
     let target=tempMatch.match(/\([^(]+\)/)[0].toString();
+    return calculate(target);
     tempMatch.replace(target,calculate(target));
   }
   returnStr += tempMatch;
