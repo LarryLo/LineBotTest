de07c14f11ac1dc1f1d3475bec08456b0d517a4b
cIII1993 <jefferychen16@gmail.com>
2017-11-05 14:24:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -405,6 +405,9 @@ function parseInput(rplyToken, inputStr) {
 //骰組function
 ////seed random
 function srand(seed_o){
+  //
+  try{
+  //
   let seed=srand(1);
   if(!isNaN(Number(seed))){
     for(let i=0;i<seed.length;i++){
@@ -417,6 +420,10 @@ function srand(seed_o){
     seed='0.'+Math.sin(seed_o).toString().substr(6);
     return seed.toString();  
   }
+  //
+  }
+  catch(error) return error.toString();
+  //
 }
 ////基本運算
 function claculater(inputStr){
