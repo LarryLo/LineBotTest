54e4961df8a25de5e3a89229c618353f60a59a50
cIII1993 <jefferychen16@gmail.com>
2017-11-05 13:45:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ function parseInput(rplyToken, inputStr) {
     return help(trigger);
   }
   //test
-  if (trigger.match(/^$/)!= null ){
+  if (trigger.match(/^#/)!= null ){
     return srand(trigger);
   }
   //SW2.0 威力骰
